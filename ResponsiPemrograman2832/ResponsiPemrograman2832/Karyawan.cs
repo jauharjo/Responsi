67461cfd7f840d27fc14e7be6e291939d6fdb51b
nurjauharmuslih <nur.muslih@students.amikom.ac.id>
2020-04-15 11:48:08 +0700

    Responsi Pemrograman 19.11.2832

---
+++ ResponsiPemrograman2832/ResponsiPemrograman2832/Karyawan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2832
+{
+    class Karyawan
+    {
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public Karyawan(string nik, string nama, int gajibulanan)
+        {
+            Nik = nik;
+            Nama = nama;
+            if (gajibulanan > 0)
+            {
+                GajiBulanan = gajibulanan;
+            }
+            else
+            {
+                gajibulanan = 0;
+            }
+        }
+    }
+}
---
+++ ResponsiPemrograman2832/ResponsiPemrograman2832/Programs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2832
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            var kar1 = new Karyawan("0123456", "Nur", 3000000);
+            var kar2 = new Karyawan("0234567", "Jauhar", 1500000);
+            var kar3 = new Karyawan("0345678", "Muslih", 2000000);
+
+            Console.WriteLine("\n=====================================================");
+            Console.WriteLine("\t\tList Gaji Bulanan");
+            Console.WriteLine("=====================================================");
+            Console.WriteLine("No   NIK \t\t Nama \t\t Gaji Bulanan");
+            Console.WriteLine("1.   {0} \t\t {1} \t\t {2}", kar1.Nik, kar1.Nama, kar1.GajiBulanan);
+            Console.WriteLine("2.   {0} \t\t {1} \t {2}", kar2.Nik, kar2.Nama, kar2.GajiBulanan);
+            Console.WriteLine("3.   {0} \t\t {1} \t {2}", kar3.Nik, kar3.Nama, kar3.GajiBulanan);
+            Console.WriteLine("\n=====================================================");
+            Console.WriteLine("\tKenaikan Gaji Karyawan Sebesar 10%");
+            Console.WriteLine("=====================================================");
+            Console.WriteLine("No   NIK \t\t Nama \t\t Gaji Bulanan");
+            Console.WriteLine("1.   {0} \t\t {1} \t\t {2}", kar1.Nik, kar1.Nama, (kar1.GajiBulanan) * 110 / 100);
+            Console.WriteLine("2.   {0} \t\t {1} \t {2}", kar2.Nik, kar2.Nama, (kar2.GajiBulanan) * 110 / 100);
+            Console.WriteLine("3.   {0} \t\t {1} \t {2}", kar3.Nik, kar3.Nama, (kar3.GajiBulanan) * 110 / 100);
+
+            Console.ReadKey();
+        }
+    }
+}
